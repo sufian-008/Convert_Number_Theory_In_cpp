f39ba52c4109a046f60f360b4c0e0e0ea27dca22
mdrafin008 <mdrafin008@gmail.com>
2023-03-06 22:08:38 +0600

    Uncomlete

---
+++ DecimalToBinary.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int DecimalToBinary(int n){
+
+   int ans = 0;
+   int x =1;
+   while(x <=n)
+
+       x*=2;
+       x/=2;
+   while(x>0){
+
+   int lastDigit = n/x;
+   n-=lastDigit*x;
+   x/=2;
+   ans = ans*10 + lastDigit;
+   }
+   return ans;
+
+}
+
+
+int main(){
+  int n;
+  cin>>n;
+
+
+  cout << DecimalToBinary(n) <<endl;
+
+}
